20658add0079a13283149f8d685840ab7c2eccd7
valeriachub <valeriachub.dev@gmail.com>
2018-08-18 12:52:55 +0300

    Initial Commit

---
+++ Food/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Valeria on 18.08.2018.
+//  Copyright © 2018 Valeria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
